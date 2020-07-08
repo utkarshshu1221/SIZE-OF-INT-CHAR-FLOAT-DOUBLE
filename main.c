3fdf24c5c137b782f4dc7a9961f1378952d9b937
utkarsh <67784611+utkarshshu1221@users.noreply.github.com>
2020-07-08 05:28:32 -0400

    Create main.c
    i have to create to show size of int,float,double,char

---
+++ main.c
@@ -0,0 +1,7 @@
+#include<iostream.h>
+intmain() {
+cout<<"size of int"<<sizeof(int)<<"bytes";
+cout<<"size of float"<<sizeof(float)<<"bytes";
+cout<<"size of double"<<sizeof(double)<<"bytes";
+cout<<"size of char"<<sizeof(char)<<"bytes";
+retuen 0;
